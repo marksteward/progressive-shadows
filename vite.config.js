cb80e8b50af13e4f11807e2f471b23ab4ae904e9
Mark Steward <marksteward@gmail.com>
2023-07-09 23:54:10 +0100

    Remove lockfile and GLB processing

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,3 @@
-import { prune, dedup, textureCompress, draco } from "@gltf-transform/functions"
-import viteCompression from "vite-plugin-compression"
-import gltf from "vite-plugin-gltf"
-import sharp from "sharp"
-
-const resolution = 1024
 export default {
   build: {
     outDir: "./docs/",
@@ -15,25 +9,6 @@ export default {
     port: 3000,
   },
   plugins: [
-    gltf({
-      transforms: [
-        // remove unused resources
-        prune(),
-
-        // combine duplicated resources
-        dedup(),
-
-        textureCompress({
-          targetFormat: "webp",
-          encoder: sharp,
-          resize: [resolution, resolution],
-        }),
-
-        // compress mesh geometry
-        draco(),
-      ],
-    }),
-
-    viteCompression(),
   ],
+  assetsInclude: ['**/*.glb'],
 }
